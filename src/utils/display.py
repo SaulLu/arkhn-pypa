a382f9dfcedfec4da6f8caba41d8369c12582eae
SaulLu <lucilesaul.com@gmail.com>
2020-03-11 14:38:49 +0100

    change numbers format

--- src/utils/display.py
+++ src/utils/display.py
@@ -6,7 +6,7 @@ def display_confusion_matrix(conf_matrix, labels_list, path=None):
     f, ax = plt.subplots()
     df_conf_matrix = pd.DataFrame(conf_matrix, labels_list, labels_list)
     sn.set(font_scale=0.8)
-    sn.heatmap(df_conf_matrix, annot=True)
+    sn.heatmap(df_conf_matrix, annot=True, fmt='d')
     plt.ylabel('True Tag')
     plt.xlabel('Predicted Tag')
     if path:
