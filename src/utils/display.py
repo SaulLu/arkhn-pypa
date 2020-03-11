2e24db0c23fbc9b2bf99e417038e7aa25b035d16
SaulLu <lucilesaul.com@gmail.com>
2020-03-11 15:39:12 +0100

    add pourcentage visualisation

--- src/utils/display.py
+++ src/utils/display.py
@@ -2,13 +2,48 @@ import pandas as pd
 import seaborn as sn
 import matplotlib.pyplot as plt
 
-def display_confusion_matrix(conf_matrix, labels_list, path=None):
-    _, __ = plt.subplots()
+def generate_confusion_matrix(conf_matrix, labels_list, curr_time=None, curr_epoch=None):
+    path_conf_mat = None
+    if curr_epoch and curr_epoch:
+        path_conf_mat = "data/parameters/img/" \
+                                + curr_time \
+                                + "_confusion_matrix" \
+                                + '_epoch_' \
+                                +  str(curr_epoch) \
+                                + ".jpeg"
+        
+        path_conf_mat_pred = "data/parameters/img/" \
+                                + curr_time \
+                                + "_confusion_matrix_pred" \
+                                + '_epoch_' \
+                                +  str(curr_epoch) \
+                                + ".jpeg"
+        
+        path_conf_mat_true = "data/parameters/img/" \
+                                + curr_time \
+                                + "_confusion_matrix_true" \
+                                + '_epoch_' \
+                                +  str(curr_epoch) \
+                                + ".jpeg"
+    
     df_conf_matrix = pd.DataFrame(conf_matrix, labels_list, labels_list)
+    display_confusion_matrix(df_conf_matrix, path=path_conf_mat, title='Confusion Marix')
+
+    df_conf_matrix_true = (df_conf_matrix.T / df_conf_matrix.T.sum()).T * 100
+    display_confusion_matrix(df_conf_matrix_true, path=path_conf_mat_true, \
+        title='Visualization of the distribution of predicted tags for a given real tag')
+    
+    df_conf_matrix_pred = df_conf_matrix / df_conf_matrix.sum() *100
+    display_confusion_matrix(df_conf_matrix_pred, path=path_conf_mat_pred, \
+        title='Visualization of the distribution of real tags for a given predicted tag')
+
+def display_confusion_matrix(df_conf_matrix, path=None, title=None):
+    _, __ = plt.subplots()
     sn.set(font_scale=0.8)
     sn.heatmap(df_conf_matrix, annot=True, fmt='d')
     plt.ylabel('True Tag', va='center')
     plt.xlabel('Predicted Tag')
+    plt.title(title)
     if path:
         try:
             plt.savefig(
