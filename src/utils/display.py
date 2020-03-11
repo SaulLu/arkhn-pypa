2fd0dddc3aa6835e2ab17c6b0b58ba6c16cef364
SaulLu <lucilesaul.com@gmail.com>
2020-03-11 14:35:55 +0100

    try fix

--- src/utils/display.py
+++ src/utils/display.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 def display_confusion_matrix(conf_matrix, labels_list, path=None):
     f, ax = plt.subplots()
     df_conf_matrix = pd.DataFrame(conf_matrix, labels_list, labels_list)
-    sn.set(font_scale=1.4)
+    sn.set(font_scale=0.8)
     sn.heatmap(df_conf_matrix, annot=True)
     plt.ylabel('True Tag')
-    plt.ylabel('Predicted Tag')
+    plt.xlabel('Predicted Tag')
     if path:
         try:
             plt.savefig(
