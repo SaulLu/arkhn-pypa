a80ede120737480b0860f6b8b0547dd48d4543ca
SaulLu <lucilesaul.com@gmail.com>
2020-03-11 14:33:30 +0100

    typo

--- src/utils/display.py
+++ src/utils/display.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 def display_confusion_matrix(conf_matrix, labels_list, path=None):
     f, ax = plt.subplots()
     df_conf_matrix = pd.DataFrame(conf_matrix, labels_list, labels_list)
-    sns.set(font_scale=1.4)
+    sn.set(font_scale=1.4)
     sn.heatmap(df_conf_matrix, annot=True)
     plt.ylabel('True Tag')
     plt.ylabel('Predicted Tag')
