5d8158d4261fd7cc41ed3d1b2df39dfd27a1b9c4
jblaval <jean-baptiste.laval@student.ecp.fr>
2020-03-15 11:45:44 +0100

    fix merge on pbs script

--- script_pbs.sh
+++ script_pbs.sh
@@ -1,5 +1,4 @@
 #PBS -S /bin/bash
-<<<<<<< HEAD
 #PBS -N test_pypa3
 #PBS -P arkhn-3a
 #PBS -l walltime=04:00:00
@@ -9,15 +8,6 @@
 #PBS -o output_pypa_1.txt
 #PBS -e error_pypa_1.txt
 #PBS -q gpuq 
-=======
-#PBS -N test_pypa1
-#PBS -P arkhn-3a
-#PBS -l walltime=01:00:00
-#PBS -M jean-baptiste.laval@student.ecp.fr
-#PBS -m be
-#PBS -o output_pypa_1.txt
-#PBS -e error_pypa_1.txt 
->>>>>>> 0cf9fd6963ae6f0877dfcaa536693c0680e15048
 
 # Go to the current directory 
 cd $PBS_O_WORKDIR
@@ -27,8 +17,4 @@ module load anaconda3/5.3.1
 source activate pypa_env
 
 # Run code
-<<<<<<< HEAD
 python ./pypa.py
-=======
-python ./pypa.py
->>>>>>> 0cf9fd6963ae6f0877dfcaa536693c0680e15048
