a7f20829ca2a86a302cd21c76f619b4c02bade6e
SaulLu <lucilesaul.com@gmail.com>
2020-03-17 12:51:45 +0100

    return path and not display it

--- src/utils/loader.py
+++ src/utils/loader.py
@@ -4,7 +4,7 @@ import os
 def get_path_last_model():
     list_of_files = glob.glob('data/parameters/intermediate/*') # * means all if need specific format then *.csv
     latest_file = max(list_of_files, key=os.path.getctime)
-    print(latest_file)
+    return(latest_file)
 
 if __name__ == "__main__":
-    get_path_last_model()
+    print(get_path_last_model())
