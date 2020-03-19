8aca2d5d487aab00f3e8b4293b6a6e952017a85f
Quentin Nativel <qmnativel@gmail.com>
2020-03-19 12:15:05 +0100

    add parsers to the main branch

---
+++ src/parsers/2014/extract_job_risk.py
@@ -0,0 +1,24 @@
+import os
+import sys
+
+import utils
+
+if __name__ == '__main__':
+    print('main')
+    if len(sys.argv) < 2:
+        raise Exception('Enter the path of the directory')
+
+    path = sys.argv[1]
+
+    if not os.path.isdir(path):
+        raise Exception('Invalid path : ', path)
+    print('path', path)
+    for filename in os.listdir(path):
+        # print(filename)
+        tags, text = utils.extract_tags_risk(os.path.join(path, filename))
+
+        words = utils.extract_words(text, tags)
+
+        df = utils.map_output(words)
+        name = filename.split('.')[0]
+        utils.save_to_csv('.', f'{name}.csv', df)
