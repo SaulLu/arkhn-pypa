b2d0cd8c4ecaf2f249f8543a9f55a188de40020c
Quentin Nativel <qmnativel@gmail.com>
2020-03-19 14:16:03 +0100

    fix

--- src/parsers/2014/extract_job_risk.py
+++ src/parsers/2014/extract_job_risk.py
@@ -13,11 +13,11 @@ if __name__ == '__main__':
     if not os.path.isdir(path):
         raise Exception('Invalid path : ', path)
     print('path', path)
-    for filename in os.listdir(path):
+    for idx, filename in enumerate(os.listdir(path)):
         # print(filename)
         tags, text = utils.extract_tags_risk(os.path.join(path, filename))
 
-        words = utils.extract_words(text, tags)
+        words = utils.extract_words(text, tags, idx)
 
         df = utils.map_output(words)
         name = filename.split('.')[0]
