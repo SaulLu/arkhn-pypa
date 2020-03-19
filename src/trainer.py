1d51716a4f618d032026293265a24ca1768f798d
SaulLu <lucilesaul.com@gmail.com>
2020-03-19 19:01:32 +0100

    print config

--- src/trainer.py
+++ src/trainer.py
@@ -30,6 +30,7 @@ class TrainModel():
 
         config, unused_kwargs = AutoConfig.from_pretrained(pretrained_model_name_or_path=self.pretrained_model, num_labels=len(tag2idx), return_unused_kwargs=True)
         assert unused_kwargs == {}
+        print(config)
         self.model = AutoModelForTokenClassification.from_config(config).to(self.device)
         print(self.model)
         self.__optimizer = self.__set_optimizer()
