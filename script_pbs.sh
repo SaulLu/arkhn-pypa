81a6d46ea34ee3d55dcfb4695000a3559fe26d4b
SaulLu <lucilesaul.com@gmail.com>
2020-03-22 13:23:52 +0100

    update pbs

--- script_pbs.sh
+++ script_pbs.sh
@@ -1,5 +1,5 @@
 #PBS -S /bin/bash
-#PBS -N test_pypa3
+#PBS -N test_pypa_dir
 #PBS -P arkhn-3a
 #PBS -l walltime=04:00:00
 #PBS -l select=1:ngpus=1:mem=30gb
@@ -13,6 +13,9 @@ cd $PBS_O_WORKDIR
 # Load the same modules as for compilation 
 module load anaconda3/5.3.1
 source activate pypa_env
+
+pip freeze | grep -v -f requirements.txt - | xargs pip uninstall -y
+pip install -r requirements.txt --upgrade
 
 # Run code
 python ./pypa.py --n_epochs 1 --pretrained_model 'bert-base-cased'
