84fdaa7a91fe6078972c1ebbe795e38f166e150f
SaulLu <lucilesaul.com@gmail.com>
2020-03-22 21:13:44 +0100

    not fullfinetuning

--- script_pbs.sh
+++ script_pbs.sh
@@ -1,5 +1,5 @@
 #PBS -S /bin/bash
-#PBS -N biobert_2009
+#PBS -N biobert_2009_not_fullfinetuning
 #PBS -P arkhn-3a
 #PBS -l walltime=04:00:00
 #PBS -l select=1:ngpus=2:mem=30gb
@@ -18,4 +18,4 @@ pip freeze | grep -v -f requirements.txt - | xargs pip uninstall -y
 pip install -r requirements.txt --upgrade
 
 # Run code
-python ./pypa.py --n_epochs 101 --pretrained_model 'monologg/biobert_v1.1_pubmed' 
+python ./pypa.py --n_epochs 101 --pretrained_model 'monologg/biobert_v1.1_pubmed' --full_finetuning False
