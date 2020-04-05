b6a9cfa8d5aa2efb81a11e81821b83caa438a795
Quentin Nativel <qmnativel@gmail.com>
2020-04-05 17:04:06 +0200

    wip

--- src/models/linear_model.py
+++ src/models/linear_model.py
@@ -7,7 +7,7 @@ class LinearModel(nn.Module):
     def __init__(self, emb_size, num_classes):
         super(LinearModel, self).__init__()
         self.l1 = nn.Linear(emb_size, num_classes)
-        self.softmax = nn.Softmax()
+        self.softmax = nn.Softmax(dim=-1)
 
     def forward(self, x):
-        return self.softmax(self.l1(x), dim=-1)
+        return self.softmax(self.l1(x), )
