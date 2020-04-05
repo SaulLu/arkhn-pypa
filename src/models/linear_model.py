afd22d006c88890d02175db7002907b9e80aca50
Quentin Nativel <qmnativel@gmail.com>
2020-04-05 15:34:37 +0200

    fix linear model

--- src/models/linear_model.py
+++ src/models/linear_model.py
@@ -1,18 +1,12 @@
-
 import torch
 from torch import nn
 
 
-
 class LinearModel(nn.Module):
 
-
-
-    def __init__(self,emb_size,  num_classes):
+    def __init__(self, emb_size, num_classes):
         super(LinearModel, self).__init__()
-        self.l1 == nn.Linear(emb_size, num_classes)
-
-
+        self.l1 = nn.Linear(emb_size, num_classes)
 
     def forward(self, x):
         return nn.Softmax(self.l1(x))
