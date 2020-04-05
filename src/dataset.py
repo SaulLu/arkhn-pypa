29756d8e4ace9c9a38b1520050e1c24d5c8cf44e
Quentin Nativel <qmnativel@gmail.com>
2020-04-05 18:11:40 +0200

    tackle .pt file in datapath

--- src/dataset.py
+++ src/dataset.py
@@ -106,6 +106,7 @@ class SentenceGetter(object):
         if os.path.isdir(data_path):
             frames = []
             for name in os.listdir(data_path):
+                if name.endswith('pt'): continue
                 frames.append(
                     pd.read_csv(os.path.join(data_path, name),
                                 encoding=encoding, engine='c').fillna(method="ffill")
