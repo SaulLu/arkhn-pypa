e296cf551a9c1cb50af50206416dee863902e33f
SaulLu <lucilesaul.com@gmail.com>
2020-04-05 14:35:26 +0200

    pbs v1

--- script_pbs.sh
+++ script_pbs.sh
@@ -1,8 +1,8 @@
 #PBS -S /bin/bash
 #PBS -N biobert_2009_fullfinetuning
 #PBS -P arkhn-3a
-#PBS -l walltime=01:00:00
-#PBS -l select=1:ngpus=1:mem=20gb
+#PBS -l walltime=08:00:00
+#PBS -l select=1:ngpus=1:mem=30gb
 #PBS -o output_pypa.txt
 #PBS -e error_pypa.txt
 #PBS -q gpuq 
@@ -19,9 +19,9 @@ source activate pypa_env
 
 # Run code
 python ./pypa.py \
+    --data_path  data/inputs/2009/dataframe_final_clean.csv \
     --n_epochs 101 \
-    --full_finetuning \
     --pretrained_model 'monologg/biobert_v1.1_pubmed' \
     --modified_model \
-    --weighted_loss \
+    --weighted_loss
     
