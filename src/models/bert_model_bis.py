c146d4b1dfa226952a75aab56c6d5417704e4513
SaulLu <lucilesaul.com@gmail.com>
2020-04-06 11:12:01 +0200

    fix typo

--- src/models/bert_model_bis.py
+++ src/models/bert_model_bis.py
@@ -167,7 +167,7 @@ class BertForTokenClassificationModified(BertPreTrainedModel):
                 active_loss = attention_mask.view(-1) == 1
                 active_logits = logits.view(-1, self.num_labels)
                 active_labels = torch.where(
-                    active_loss, labels.view(-1), torch.tensor(loss_fct.ignore_index).type_as(labels)
+                    active_loss, labels.view(-1), torch.tensor(self.ignore_index).type_as(labels)
                 )
             else:
                 active_logits = logits.view(-1, self.num_labels)
