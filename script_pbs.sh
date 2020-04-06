f18be7578248152047243c3a39d62e669ff9297d
SaulLu <lucilesaul.com@gmail.com>
2020-04-07 00:03:51 +0200

    pbs v2 6

--- script_pbs.sh
+++ script_pbs.sh
@@ -22,7 +22,5 @@ python ./pypa.py \
     --data_path  data/inputs/2009/dataframe_final_clean.csv \
     --n_epochs 1001 \
     --pretrained_model 'monologg/biobert_v1.1_pubmed' \
-    --modified_model \
     --full_finetuning \
-    --weighted_loss 'less_out'\
     --batch_size 100
