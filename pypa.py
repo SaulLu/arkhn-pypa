5393861ab4083730c856659a80096888463257db
Quentin Nativel <qmnativel@gmail.com>
2020-04-08 14:12:10 +0200

    fix flair with loss_test_4 branch

--- pypa.py
+++ pypa.py
@@ -232,7 +232,7 @@ def __dataloader(dataset, val_size, test_size, batch_size):
     valid_sampler = SubsetRandomSampler(val_indices)
     test_sampler = SubsetRandomSampler(test_indices)
 
-    tags_train = dataset[train_indices][2]
+    tags_train = dataset[train_indices][2] if isinstance(dataset,NerDataset) else dataset[train_indices][1]
     num_items = Counter(torch.flatten(tags_train).cpu().numpy())
 
     max_num_items = max(num_items.values())
