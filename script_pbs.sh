64ea46f18e4a95a391c588405525d6e905d4c270
SaulLu <lucilesaul.com@gmail.com>
2020-04-10 18:27:52 +0200

    pbs ignore out

--- script_pbs.sh
+++ script_pbs.sh
@@ -27,4 +27,4 @@ python ./pypa.py \
     --batch_size 100 \
     --pretrained_model 'monologg/biobert_v1.1_pubmed' \
     --modified_model \
-    --weighted_loss 'global' \
+    --weighted_loss 'ignore_out' \
